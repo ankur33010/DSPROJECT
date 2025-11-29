7135514825a31f3e0552d1c40371cbb0529b5545
ankur33010 <114218720+ankur33010@users.noreply.github.com>
2025-11-29 18:28:11 +0530

    Data Ingestion intiated

--- src/logger.py
+++ src/logger.py
@@ -1,17 +1,17 @@
-import logging 
+import logging
 import os
 from datetime import datetime
 
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE= f"logs/log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-LOGS_PATH= os.path.join(os.getcwd(), "logs",LOG_FILE)
-os.makedirs(LOGS_PATH,exist_ok=True)
-
-LOGS_FILE_PATH= os.path.join(LOGS_PATH, LOG_FILE)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-    filename=LOGS_FILE_PATH,
-    format="[%(asctime)s] %(levelname)s - %(message)s",
-    level=logging.INFO,)
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 
 
+)
